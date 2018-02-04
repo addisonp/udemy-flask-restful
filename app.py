2fc84fdae11e017c5b467c95f7f90384ade16a7b
addison.pan <addisonp@gmail.com>
2018-02-03 22:27:06 -0800

    [Flask-restful]  - request parsing to ignore non defined variables

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
@@ -50,7 +50,18 @@ class Item(Resource):
         return {'message': 'Item deleted'}
 
     def put(self, name):
-        data = request.get_json()
+        parser = reqparse.RequestParser()  # run the request through it
+        # list all possible arguments that we will be keeping/using
+        # if the argument is not listed in the parser.add_arguments, it will be erased and not saved
+        parser.add_argument('price',
+                            type=float,
+                            required=True,
+                            help="This field cannot be left blank!"
+                            )
+
+        # data = request.get_json()
+        data = parser.parse_args()
+
         item = next(filter(lambda x: x['name'] == name, items), None)
         if item is None:
             item = {'name': name, 'price': data['price']}
