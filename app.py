9ac342668fd8a5ef3098678cedc39b8feeca08bc
addison.pan <addisonp@gmail.com>
2018-02-03 18:59:46 -0800

    [Flask-restful]  - use dict comprehension  - use safe_str_cmp for string comparisions (python 2.7)  - create a User class

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
+app.secret_key = 'jose'
 api = Api(app)
+
 
 # save internally for testing
 items = []
