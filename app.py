5d57f3744c6607ead2093dd4163a24e68705bf18
addison.pan <addisonp@gmail.com>
2018-02-03 16:42:57 -0800

    [Flask-restful]  - use the request.get_json to retrieve the POST data passed

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
@@ -13,14 +13,22 @@ class Item(Resource):
         for item in items:
             if item['name'] == name:
                 return item
-        return {"item": None}, 404 # error code for not found is 404
+        return {"item": None}, 404  # error code for not found is 404
 
     def post(self, name):
-        item = {'name': name, 'price': 12.00}
+        data = request.get_json()
+
+        item = {'name': name, 'price': data['price']}
         items.append(item)
-        return item, 201 # created code
+        return item, 201  # created code
+
+
+class Items(Resource):
+    def get(self):
+        return {"items": items}
 
 
 api.add_resource(Item, '/item/<string:name>')
+api.add_resource(Items, '/items')
 
-app.run(port=5000)
+app.run(port=5000, debug=True)
