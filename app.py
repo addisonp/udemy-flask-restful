222e7b7f192b6502a6e12a106f7476794428ea1d
addison.pan <addisonp@gmail.com>
2018-02-03 19:39:56 -0800

    [Flask-restful]  - authentication

--- app.py
+++ app.py
@@ -1,19 +1,34 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
 app.secret_key = 'jose'
 api = Api(app)
 
 
+jwt = JWT(app, authenticate, identity)# JWT creates a new end point /auth
+# we send /auth a username/password and that is sent to the authenticate function
+# if the u/p is valid the authenticate returns the user and that becomes the identity
+# the auth endpoint returns a JWT token
+
+# we can send the JWT token to the next request that we make
+# when we send a JWT token, is that the JWT calls the identity function and uses the JWT token
+#   to get the userid and the correct user for the userid that the JWT token represents
+#   if it can do that, then the user was authenticated, the JWT token is valid
+#   this is done with flask_jwt.jwt_required
+
 # save internally for testing
 items = []
 
 
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
-        # return {"item": item}, 200 if ite is not None else 404
+        # return {"item": item}, 200 if it is not None else 404
         return {"item": item}, 200 if item else 404
         # for item in items:
         #     if item['name'] == name:
--- security.py
+++ security.py
@@ -23,7 +23,8 @@ username_mapping = {u.username: u for u in users}
 #     'password': 'asdf'
 # }}
 
-userid_mapping = {u.id: u for u in users)
+userid_mapping = {u.id: u for u in users}
+
 
 # userid_mapping = {1: {
 #     'id': 1,
@@ -39,7 +40,7 @@ def authenticate(username, password):
     if user and safe_str_cmp(user.password, password):
         return user
 
+
 def identity(payload):
     user_id = payload['identity']
     return userid_mapping.get(user_id, None)
-
