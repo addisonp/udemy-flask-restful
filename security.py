3c6c7e4b8b187a826c97bf6613fa897cae531ec1
addison.pan <addisonp@gmail.com>
2018-02-04 21:03:10 -0800

    [Flask-restful]  - convert the database from being in memory to a SQLite db  - use the database for authentication

--- security.py
+++ security.py
@@ -1,46 +1,14 @@
 from user import User
 from werkzeug.security import safe_str_cmp
 
-# users = [
-#     {
-#         'id': 1,
-#         'username': 'bob',
-#         'password': 'asdf'
-#     }
-# ]
-
-# create a class to store the user data
-users = [
-    User(1, 'bob', 'asdf')
-]
-
-# then we can use dict comprehension
-username_mapping = {u.username: u for u in users}
-
-# username_mapping = {'bob': {
-#     'id': 1,
-#     'username': 'bob',
-#     'password': 'asdf'
-# }}
-
-userid_mapping = {u.id: u for u in users}
-
-
-# userid_mapping = {1: {
-#     'id': 1,
-#     'username': 'bob',
-#     'password': 'asdf'
-# }}
 
 # function to get the user details without having to iterate over the users list
 def authenticate(username, password):
-    user = username_mapping.get(username, None)
-    # if user and user.password == password:
-    # python 2.7, not safe to use == for string comparision, use safe_str_cmp to not worry about encodings
+    user = User.find_by_username(username)
     if user and safe_str_cmp(user.password, password):
         return user
 
 
 def identity(payload):
     user_id = payload['identity']
-    return userid_mapping.get(user_id, None)
+    return User.find_by_id(user_id)
--- user.py
+++ user.py
@@ -1,5 +1,48 @@
+import sqlite3
+
+
 class User:
     def __init__(self, _id, username, password):
         self.id = _id
         self.username = username
         self.password = password
+
+    @classmethod
+    def find_by_username(cls, username):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM users WHERE username=?"
+        # need to pass a tuple (value,)
+        result = cursor.execute(query, (username,))
+
+        row = result.fetchone()
+        if row:
+            user = cls(*row)# we can use #row since row[0] is the id, row[1] is the username and #row[2] is the password
+        else:
+            user = None
+
+        connection.close()
+        return user
+
+    @classmethod
+    def find_by_id(cls, _id):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM users WHERE id=?"
+        # need to pass a tuple (value,)
+        result = cursor.execute(query, (_id,))
+
+        row = result.fetchone()
+        if row:
+            user = cls(*row)
+        else:
+            user = None
+
+        connection.close()
+        return user
+
+    # tested with POSTMAN where the Header contains a key: Authorization
+    # and a value: JWT <token>
+    # <token> is retrieved from /auth where we pass the u/p
