412607e22b1993caa47305444389bd9f8162ab9c
addison.pan <addisonp@gmail.com>
2018-02-17 19:50:02 -0800

    [Flask-restful]  - section 6  - SQLAlchemy add new model/resources and methods

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from resources.item import Item, Items
 from security import authenticate, identity
 
 from resources.user import UserRegister
+from resources.store import Store, StoreList
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
@@ -33,6 +34,8 @@ jwt = JWT(app, authenticate, identity)  # JWT creates a new end point /auth
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(Items, '/items')
 api.add_resource(UserRegister, '/register')
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(StoreList, '/stores')
 
 if __name__ == '__main__':
     from db import db
